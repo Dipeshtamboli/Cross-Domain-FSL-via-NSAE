6a30783916e2713be4819555b1ef0a597b51a8b0
Dipeshtamboli <dipeshtamboli@gmail.com>
2023-02-19 18:02:24 -0500

    dataloader

---
+++ configs.py
@@ -0,0 +1,8 @@
+save_dir = './models'
+
+miniImageNet_path = './data/miniImagenet/train'
+
+ISIC_path = "/ssd/data/ISIC"
+ChestX_path = "/ssd/data/chestX"
+CropDisease_path = "/ssd/data/CropDiseases"
+EuroSAT_path = "/ssd/data/EuroSAT/2750"
